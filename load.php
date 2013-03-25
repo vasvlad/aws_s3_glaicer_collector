c9b673cf015add72d4099af030c5027c3cabe2b0
Vlad Vasilyeu <vasvlad@gmail.com>
2013-03-25 10:52:26 +0300

    fixed path for Vlad

--- load.php
+++ load.php
@@ -1,11 +1,13 @@
 <?php
 $output=array();
 $returnVar=0;
-chdir("/opt/r2/vlad/aws_s3_glaicer_collector");
-exec('git pull 2&gt;&1', $output , $returnVar);
+#chdir("/opt/r2/vlad/aws_s3_glaicer_collector");
+chdir("/var/www/aws_s3_glaicer");
+//exec('git pull 2&gt;&1', $output , $returnVar);
+exec('git pull ', $output , $returnVar);
 echo "<pre>\n";
 echo "return status: $returnVar\n\n";
-print_r($output);
+//print_r($output);
 echo "</pre>\n";
 //cd /opt/r2/vlad/aws_s3_glaicer_collector && git pull
 
